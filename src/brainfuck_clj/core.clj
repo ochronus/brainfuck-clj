4769503931db85ca73afb1863c9a1b176a7c1268
Csaba Okrona <ochronus@ochronus.com>
2014-03-06 11:32:30 +0100

    refactoring

--- src/brainfuck_clj/core.clj
+++ src/brainfuck_clj/core.clj
@@ -3,37 +3,37 @@
 
 (defn bf-interpreter [program-code]
     (let [
-        find-bracket (fn [opening-bracket closing-bracket ip direction]
-            (loop [i (direction ip) opened 0]
+        find-bracket (fn [opening-bracket closing-bracket instruction-pointer direction]
+            (loop [i (direction instruction-pointer) opened 0]
                 (condp = (nth program-code i)
                     opening-bracket (recur (direction i) (inc opened))
                     closing-bracket (if (zero? opened) i (recur (direction i) (dec opened)))
                     (recur (direction i) opened))))]
 
-        (loop [cells [0N], current-cell 0, ip 0]
-            (condp = (get program-code ip)
-                \<  (recur cells (dec current-cell) (inc ip))
-                \+  (recur (update-in cells [current-cell] inc) current-cell (inc ip))
-                \-  (recur (update-in cells [current-cell] dec) current-cell (inc ip))
+        (loop [cells [0N], current-cell 0, instruction-pointer 0]
+            (condp = (get program-code instruction-pointer)
+                \<  (recur cells (dec current-cell) (inc instruction-pointer))
+                \+  (recur (update-in cells [current-cell] inc) current-cell (inc instruction-pointer))
+                \-  (recur (update-in cells [current-cell] dec) current-cell (inc instruction-pointer))
 
                 \>  (let [
                             next-ptr (inc current-cell)
                             next-cells (if (= next-ptr (count cells)) 
                                             (conj cells 0N) 
                                              cells)]
-                        (recur next-cells next-ptr (inc ip)))
+                        (recur next-cells next-ptr (inc instruction-pointer)))
 
                 \.  (do
                         (print (char (nth cells current-cell)))
-                        (recur cells current-cell (inc ip)))
+                        (recur cells current-cell (inc instruction-pointer)))
                 \,  (let [ch (.read System/in)]
-                        (recur (assoc cells current-cell ch) current-cell (inc ip)))
+                        (recur (assoc cells current-cell ch) current-cell (inc instruction-pointer)))
                 \[  (recur cells current-cell (inc (if (zero? (nth cells current-cell))
-                        (find-bracket \[ \] ip inc)
-                        ip)))
-                \]  (recur cells current-cell (find-bracket \] \[ ip dec))
+                        (find-bracket \[ \] instruction-pointer inc)
+                        instruction-pointer)))
+                \]  (recur cells current-cell (find-bracket \] \[ instruction-pointer dec))
                 nil cells
-                (recur cells current-cell (inc ip))))))
+                (recur cells current-cell (inc instruction-pointer))))))
 
 (defn -main [& args]
   (if (nth args 0)
