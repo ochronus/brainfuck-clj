0f404bb46e80889b03a61b1ce79dd3d8b8d54fd7
Yehonathan Sharvit <viebel@gmail.com>
2016-12-18 22:34:08 +0200

    Update core.clj

--- src/brainfuck_clj/core.clj
+++ src/brainfuck_clj/core.clj
@@ -5,5 +5,5 @@
 
 (defn -main [& args]
   (if (nth args 0)
-    (bf-interpreter (slurp (nth args 0)))
+    (bf-interpreter (slurp (nth args 0)) #(.read System/in))
     (println "Please specify a brainfuck file as the first argument")))
